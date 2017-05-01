90b138ef32a48f5e53c8676376b05b08ec78bc7f
akito <akito.tsutsui@gmail.com>
2017-05-01 18:37:13 +0900

    first commit

---
+++ db/migrate/20170501082314_create_photos.rb
@@ -0,0 +1,10 @@
+class CreatePhotos < ActiveRecord::Migration
+  def change
+    create_table :photos do |t|
+      t.references :listing, index: true
+
+      t.timestamps null: false
+    end
+    add_foreign_key :photos, :listings
+  end
+end
