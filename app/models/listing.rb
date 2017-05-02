91b886547246fba2f067671e1a05f695ab403952
akito <akito.tsutsui@gmail.com>
2017-05-02 18:08:04 +0900

    add Review method

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,9 @@ class Listing < ActiveRecord::Base
   belongs_to :user
   has_many :photos
   has_many :reservations
-  # 必須項目
+  has_many :reviews
+
+  #必須項目
   validates :home_type, presence: true
   validates :pet_type, presence: true
   validates :pet_size, presence: true
@@ -10,4 +12,8 @@ class Listing < ActiveRecord::Base
 
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
+  
+  def average_star_rate
+    reviews.count == 0 ? 0 : reviews.average(:rate).round(1)
+  end
 end
---
+++ db/migrate/20170502073614_create_reviews.rb
@@ -0,0 +1,15 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.text :description
+      t.integer :rate
+      t.references :listing, index: true
+      t.references :user, index: true
+
+      t.timestamps null: false
+    end
+    add_foreign_key :reviews, :listings
+    add_foreign_key :reviews, :userss
+    
+  end
+end
